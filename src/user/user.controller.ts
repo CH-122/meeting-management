34f41c8b975b6f761d743f8acc606b9f9c826c34
CH-122 <1521930938@qq.com>
2024-05-24 15:12:59 +0800

    feat: 用户登录

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {
+    console.log(1);
+  }
 
   @Post('sign_up')
   async signUp(@Body() createUserDto: CreateUserDto) {
@@ -15,5 +18,27 @@ export class UserController {
   @Post('send_captcha')
   async sendCaptcha(@Body('email') email: string) {
     return await this.userService.sendCaptcha(email);
+  }
+
+  /**
+   * c 端登录
+   * @param loginUserDto
+   * @returns
+   */
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    console.log(loginUserDto);
+    return await this.userService.login(loginUserDto);
+  }
+
+  /**
+   * 管理端登录
+   * @param loginUserDto
+   * @returns
+   */
+  @Post('admin_login')
+  async adminLogin(@Body() loginUserDto: LoginUserDto) {
+    console.log(loginUserDto);
+    return await this.userService.login(loginUserDto, true);
   }
 }
